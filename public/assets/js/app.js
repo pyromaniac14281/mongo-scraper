ad59f22b952ce1d0ca820a7aa8ddef92cf908b28
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-02 17:19:28 -0400

    article.js in progress

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,13 +8,11 @@ $("#scrape").on("click", function() {
     })
 });
 
-//Set clicked nav option to active
 $(".navbar-nav li").click(function() {
    $(".navbar-nav li").removeClass("active");
    $(this).addClass("active");
 });
 
-//Handle Save Article button
 $(".save").on("click", function() {
     var thisId = $(this).attr("data-id");
     $.ajax({
@@ -25,7 +23,6 @@ $(".save").on("click", function() {
     })
 });
 
-//Handle Delete Article button
 $(".delete").on("click", function() {
     var thisId = $(this).attr("data-id");
     $.ajax({
@@ -36,7 +33,6 @@ $(".delete").on("click", function() {
     })
 });
 
-//Handle Save Note button
 $(".saveNote").on("click", function() {
     var thisId = $(this).attr("data-id");
     if (!$("#noteText" + thisId).val()) {
@@ -59,7 +55,6 @@ $(".saveNote").on("click", function() {
     }
 });
 
-//Handle Delete Note button
 $(".deleteNote").on("click", function() {
     var noteId = $(this).attr("data-note-id");
     var articleId = $(this).attr("data-article-id");
